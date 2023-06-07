751dbf6d6ca5adf020b94e50f9068511fe938380
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2023-06-08 03:32:21 +0900

    取引先新規登録Formの項目だけ定義

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -11,8 +11,49 @@ export default function Account() {
         <title>取引先新規登録</title>
       </Head>
       <DefaultLayout>
-        <h1>this is new account</h1>
-        <div className='font-bold'> this is new account decorated</div>
+        <h1>新規取引先の登録</h1>
+        <form className="mx-auto">
+          <div>
+            <label htmlFor="accountName">取引先名</label>
+            <input className="border" type="text" id="accountName" />
+          </div>
+          <div>
+            <label htmlFor="accountBillingPostalCode">郵便番号(請求先)</label>
+            <input className="border" type="text" id="accountBillingPostalCode" />
+          </div>
+          <div>
+            <label htmlFor="accountBillingState">都道府県(請求先)</label>
+            <input className="border" type="text" id="accountBillingState" />
+          </div>
+          <div>
+            <label htmlFor="accountBillingCity">市区町村(請求先)</label>
+            <input className="border" type="text" id="accountBillingCity" />
+          </div>
+          <div>
+            <label htmlFor="accountBillingStreet">番地その他(請求先)</label>
+            <input className="border" type="text" id="accountBillingStreet" />
+          </div>
+          <div>
+            <label htmlFor="accountShippingPostalCode">郵便番号(納品先)</label>
+            <input className="border" type="text" id="accountShippingPostalCode" />
+          </div>
+          <div>
+            <label htmlFor="accountShippingState">都道府県(納品先)</label>
+            <input className="border" type="text" id="accountShippingState" />
+          </div>
+          <div>
+            <label htmlFor="accountShippingCity">市区町村(納品先)</label>
+            <input className="border" type="text" id="accountShippingCity" />
+          </div>
+          <div>
+            <label htmlFor="accountShippingStreet">番地その他(納品先)</label>
+            <input className="border" type="text" id="accountShippingStreet" />
+          </div>
+          <div>
+            <label htmlFor="accountPhone">電話番号</label>
+            <input className="border" type="text" id="accountPhone" />
+          </div>
+        </form>
       </DefaultLayout>
     </>
   )
