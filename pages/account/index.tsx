0533358e5a3b24d9c5ec92d6fa2526811e9f710f
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2023-06-12 23:22:21 +0900

    画面の項目を整列

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -12,46 +12,46 @@ export default function Account() {
       </Head>
       <DefaultLayout>
         <h1>新規取引先の登録</h1>
-        <form className="mx-auto">
-          <div>
-            <label htmlFor="accountName">取引先名</label>
-            <input className="border" type="text" id="accountName" />
+        <form className="flex flex-col">
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0"htmlFor="accountName">取引先名</label>
+            <input className="border flex-grow" type="text" id="accountName" />
           </div>
-          <div>
-            <label htmlFor="accountBillingPostalCode">郵便番号(請求先)</label>
-            <input className="border" type="text" id="accountBillingPostalCode" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountBillingPostalCode">郵便番号(請求先)</label>
+            <input className="border flex-grow" type="text" id="accountBillingPostalCode" />
           </div>
-          <div>
-            <label htmlFor="accountBillingState">都道府県(請求先)</label>
-            <input className="border" type="text" id="accountBillingState" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountBillingState">都道府県(請求先)</label>
+            <input className="border flex-grow" type="text" id="accountBillingState" />
           </div>
-          <div>
-            <label htmlFor="accountBillingCity">市区町村(請求先)</label>
-            <input className="border" type="text" id="accountBillingCity" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountBillingCity">市区町村(請求先)</label>
+            <input className="border flex-grow" type="text" id="accountBillingCity" />
           </div>
-          <div>
-            <label htmlFor="accountBillingStreet">番地その他(請求先)</label>
-            <input className="border" type="text" id="accountBillingStreet" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountBillingStreet">番地その他(請求先)</label>
+            <input className="border flex-grow" type="text" id="accountBillingStreet" />
           </div>
-          <div>
-            <label htmlFor="accountShippingPostalCode">郵便番号(納品先)</label>
-            <input className="border" type="text" id="accountShippingPostalCode" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountShippingPostalCode">郵便番号(納品先)</label>
+            <input className="border flex-grow" type="text" id="accountShippingPostalCode" />
           </div>
-          <div>
-            <label htmlFor="accountShippingState">都道府県(納品先)</label>
-            <input className="border" type="text" id="accountShippingState" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountShippingState">都道府県(納品先)</label>
+            <input className="border flex-grow" type="text" id="accountShippingState" />
           </div>
-          <div>
-            <label htmlFor="accountShippingCity">市区町村(納品先)</label>
-            <input className="border" type="text" id="accountShippingCity" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountShippingCity">市区町村(納品先)</label>
+            <input className="border flex-grow" type="text" id="accountShippingCity" />
           </div>
-          <div>
-            <label htmlFor="accountShippingStreet">番地その他(納品先)</label>
-            <input className="border" type="text" id="accountShippingStreet" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountShippingStreet">番地その他(納品先)</label>
+            <input className="border flex-grow" type="text" id="accountShippingStreet" />
           </div>
-          <div>
-            <label htmlFor="accountPhone">電話番号</label>
-            <input className="border" type="text" id="accountPhone" />
+          <div className="flex items-center">
+            <label className="w-40 flex-shrink-0" htmlFor="accountPhone">電話番号</label>
+            <input className="border flex-grow" type="text" id="accountPhone" />
           </div>
         </form>
       </DefaultLayout>
