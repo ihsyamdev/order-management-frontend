732392bd76453082512729a07b8e9f356e2bfccd
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2023-07-02 01:08:10 +0900

    OrderDetailの追加機能を実装

--- components/order/DetailAddButton/index.tsx
+++ components/order/DetailAddButton/index.tsx
@@ -1,8 +1,14 @@
 import styles from './styles.module.css'
+import { OrderDetail } from '@/types/order'
 
-const DetailAddButton = () => {
+interface DetailAddButtonProps {
+  handleClick?: () => void
+}
+
+const DetailAddButton: React.FC<DetailAddButtonProps> = ({handleClick}) => {
+
   return (
-    <button className={styles.button}>商品を追加</button>
+    <button className={styles.button} onClick={handleClick}>商品を追加</button>
   )
 }
 
--- pages/order/new/index.tsx
+++ pages/order/new/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import { useState } from 'react'
 import { DefaultLayout } from '@/components/layouts/DefaultLayout'
 import OrderForm from '@/components/order/OrderForm'
 import DraftSaveButton from '@/components/order/DraftSaveButton'
@@ -8,29 +8,39 @@ import DetailRemoveButton from '@/components/order/DetailRemoveButton'
 import OrderDetailList from '@/components/order/OrderDetailList'
 import { OrderDetail } from '@/types/order'
 
-const orderDetails: Partial<OrderDetail>[] = [
-  {
-    id: "A001",
-    name: '商品A',
-    unitPrice: 1000,
-    quantity: 1,
-    taxRate: 10
-  },
-  {
-    id: "A002",
-    name: '商品B',
-    unitPrice: 2000,
-    quantity: 2,
-    taxRate: 10
-  }
-]
 
 export default function Order() {
 
-  const handleSave = () => {
-    console.log('saved')
-  }
+  const [orderDetails, setOrderDetails] = useState<Partial<OrderDetail>[]>([
+    {
+      id: "A001",
+      name: '商品A',
+      unitPrice: 1000,
+      quantity: 1,
+      taxRate: 10
+    },
+    {
+      id: "A002",
+      name: '商品B',
+      unitPrice: 2000,
+      quantity: 2,
+      taxRate: 10
+    }
+  ])
 
+  const handleClick = () => {
+    setOrderDetails([
+      ...orderDetails,
+      {
+        id: '',
+        name: '',
+        unitPrice: 0,
+        quantity: 0,
+        taxRate: 0
+      }
+    ])
+    console.log(orderDetails)
+  }
 
   return (
     <>
@@ -41,7 +51,7 @@ export default function Order() {
         <h1 className='p-6 text-xl font-bold'>新規受注の登録</h1>
         <DraftSaveButton />
         <OrderForm />
-        <DetailAddButton />
+        <DetailAddButton handleClick={handleClick} />
         <DetailRemoveButton />
         <OrderDetailList orderDetails={orderDetails}/>
       </DefaultLayout>
